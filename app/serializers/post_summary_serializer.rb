14fbce4eee916bc495d167bacebfe71e45d70683
kaye garcia <kayemgarcia@gmail.com>
2021-12-21 11:35:59 -0600

    commit tree 459ea6e5eef5b45d9ec9998f1e2617eef072a56d

---
+++ app/serializers/post_summary_serializer.rb
@@ -0,0 +1,7 @@
+# class PostSummarySerializer < ActiveModel::Serializer
+#   attributes :summary
+
+#   def summary
+#     "#{self.object.title} - #{self.object.description[0..39]}..."
+#   end
+# end
